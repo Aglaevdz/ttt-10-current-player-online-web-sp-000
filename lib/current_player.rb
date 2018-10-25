4280397d32dcd5e200ff2269ea3d1c434adeccfa
Aglae Verduzco <aglae.cv@gmail.com>
2018-10-25 00:17:41 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,5 @@
 def turn_count(board)
-  board = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
     counter = 0
     board.each do |board|
     counter += 1
